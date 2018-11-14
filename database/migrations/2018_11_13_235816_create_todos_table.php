ba4ef3c9de5425b474a55462139d3398ecc18a20
Rachel <rachel.loziuk@gmail.com>
2018-11-14 10:37:06 -0600

    add a default value to database migration

--- database/migrations/2018_11_13_235816_create_todos_table.php
+++ database/migrations/2018_11_13_235816_create_todos_table.php
@@ -16,7 +16,7 @@ class CreateTodosTable extends Migration
         Schema::create('todos', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->boolean('completed');
+            $table->boolean('completed')->default(false);
             $table->timestamps();
         });
     }
